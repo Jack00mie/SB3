73bde9a1f4095da13d9f9b80e8479073ab67c5ca
LeonP <leon.pueschel@gmx.de>
2025-02-11 16:41:04 +0100

    Actions with values

--- httpEnvironment.py
+++ httpEnvironment.py
@@ -2,7 +2,7 @@ import os
 import signal
 import threading
 from uuid import UUID
-from typing import Dict, Union, Optional
+from typing import Dict, Union, Optional, Tuple, List
 import copy
 from collections import deque
 import random
@@ -14,9 +14,12 @@ from pydantic import BaseModel, Field
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.callbacks import BaseCallback, EveryNTimesteps
+from stable_baselines3.common.preprocessing import preprocess_obs
 import torch as th
 from fastapi import BackgroundTasks, FastAPI, Response
+from fastapi.responses import JSONResponse
 from stable_baselines3.common.policies import BasePolicy
+from stable_baselines3.common.utils import obs_as_tensor
 
 # start with: uvicorn httpEnvironment:app --host 0.0.0.0 --port 8095
 # start TensorBoard with: tensorboard --logdir ./logs
@@ -44,15 +47,15 @@ class SelfPlayParameters(BaseModel):
     The Parameters used for self play.
     """
     policyWindowSize: int = Field("How many old policy's/ models should be saved.", gt=0)
-    addPolicyEveryXSteps: int = Field("Add current the policy every X steps. Determines how old policy should be are.", gt=0)
+    addPolicyEveryXSteps: int = Field("Add current the policy every X steps. Determines how old policy should be are.",
+                                      gt=0)
     useLatestPolicy: float = Field("How often should the latest policy be used for self play.", ge=0, le=1)
-
 
 
 class Agent:
     agent_id: UUID
     baseAlgorithm: BaseAlgorithm
-    self_play_policy: deque[BasePolicy] = None
+    self_play_polices: deque[BasePolicy] = None
     latest_policy: BasePolicy = None
     use_self_play: bool = False
     callback: BaseCallback = None
@@ -63,13 +66,16 @@ class Agent:
     def __init__(self, agent_id: UUID, model: BaseAlgorithm, self_play_parameters: [SelfPlayParameters | None] = None):
         self.agent_id = agent_id
         self.baseAlgorithm = model
-        print(self_play_parameters)
+
         if self_play_parameters is not None:
             self.use_self_play = True
-            self.self_play_policy = deque(maxlen=self_play_parameters.policyWindowSize - 1)
             self.latest_policy = copy.deepcopy(self.baseAlgorithm.policy)
+            self.self_play_polices = deque(maxlen=self_play_parameters.policyWindowSize - 1)
+            for _ in range(self_play_parameters.policyWindowSize - 1):
+                self.self_play_polices.append(self.latest_policy)
             add_to_self_play = AddToSelfPlay(self)
-            self.callback = EveryNTimesteps(n_steps=self_play_parameters.addPolicyEveryXSteps, callback=add_to_self_play)
+            self.callback = EveryNTimesteps(n_steps=self_play_parameters.addPolicyEveryXSteps,
+                                            callback=add_to_self_play)
             self.use_latest_policy = self_play_parameters.useLatestPolicy
 
     @classmethod
@@ -114,25 +120,55 @@ class Agent:
         print("Training complete.")
 
     def self_play(self, observation: np.ndarray):
-        if random.random() < self.use_latest_policy or len(self.self_play_policy) == 0:
+        if random.random() < self.use_latest_policy:
             policy = self.latest_policy
-            return policy.predict(observation)
-        policy = random.choice(self.self_play_policy)
-
+        else:
+            policy = random.choice(self.self_play_polices)
         action = policy.predict(observation)
-        action2 = self.baseAlgorithm.predict(observation)
-        print(f"{action[0]} {action2[0]}")
-        print(action2[0] == action[0])
+
+        print(action)
+
         return action
+
+    def predict_values(self, observation: np.ndarray) -> np.ndarray:
+        if type(self.baseAlgorithm) is DQN:
+            with th.no_grad():
+                tensor = self.baseAlgorithm.policy.obs_to_tensor(observation)[0]
+                values = self.baseAlgorithm.policy.q_net(tensor)
+                values = values.detach().to('cpu').numpy()
+        elif type(self.baseAlgorithm) is PPO:
+            with th.no_grad():
+                tensor = self.baseAlgorithm.policy.obs_to_tensor(observation)[0]
+                distribution = self.baseAlgorithm.policy.get_distribution(tensor)
+                probs = distribution.distribution.probs
+                values = probs.detach().to('cpu').numpy()
+
+        print(f"action values: {values[0]}")
+        return values[0]
+
+    def predict(self, observation: np.ndarray, available_actions: np.ndarray) -> tuple[int, list[float]]:
+        values = self.predict_values(observation)
+        available_values: list[float] = []
+        best_action: int = available_actions[0]
+
+        for available_action in available_actions:
+            if values[available_action] > values[best_action]:
+                best_action = available_action
+            available_values.append(float(values[available_action]))
+
+        print(f"availbae actions {available_actions}")
+        print(values)
+        print(available_values)
+        print(best_action)
+
+        return int(best_action), available_values
 
 
 class AddToSelfPlay(BaseCallback):
     def _on_step(self) -> bool:
-        self.agent.self_play_policy.append(self.agent.latest_policy)
+        self.agent.self_play_polices.append(self.agent.latest_policy)
         with self.agent.use_model_lock:
             self.agent.latest_policy = copy.deepcopy(self.model.policy)
-
-        print("Hello Copyed HHHEEEEEEEEEEEEEEEELLLLO")
         return True
 
     def __init__(self, agent: Agent, verbose: int = 0):
@@ -140,10 +176,9 @@ class AddToSelfPlay(BaseCallback):
         self.agent = agent
 
 
-
-
 # TODO: WHere put env and trainer?
 agents: dict[UUID, Agent] = dict()
+
 
 class EnvironmentParameters(BaseModel):
     """
@@ -160,16 +195,18 @@ class HttpEnv(gym.Env):
         # self.observation_vector_size = observation_vector_size
 
         # self.observation_space = gym.spaces.Box(low=0, high=1, dtype=np.float32,
-                                              #  shape=(self.observation_vector_size,))  # TODO: for differen obs
-        self.observation_space = gym.spaces.MultiDiscrete(environment_parameters.observationRangeSizes, start=environment_parameters.observationRangeStarts)
+        #  shape=(self.observation_vector_size,))  # TODO: for differen obs
+        self.observation_space = gym.spaces.MultiDiscrete(environment_parameters.observationRangeSizes,
+                                                          start=environment_parameters.observationRangeStarts)
         self.action_space = gym.spaces.Discrete(environment_parameters.actionSpaceSize)
 
     def reset(self, *, seed: int | None = None, options=None, ):
         super().reset(seed=seed)
 
+        print("1")
         response = requests.post(f"http://127.0.0.1:{JAVA_PORT}/reset")
         response_body = response.json()
-
+        print("2")
         observation_vector = np.array(response_body["observationVector"])
         info = response_body["info"]
         # print("reset: ")
@@ -199,14 +236,19 @@ class SB3Parameters(BaseModel):
     """
     The Parameters used to creat a gym.env and a stable baseline agent.
     """
-    agent_id: UUID = Field(description="The agents Id used for loading and saving and also impotent when several agents are used.")
+    agent_id: UUID = Field(
+        description="The agents Id used for loading and saving and also impotent when several agents are used.")
     agentType: str = Field(description="Type of sb3 agent that should be created.")  # TODO: enums
 
-    baseParameters: Dict[str, Union[str, int, float, bool, None]] = Field(description="Parameters every sb3 agent has in common.")
-    agentParameters: Dict[str, Union[str, int, float, bool, None]] = Field(description="Special parameters specific to agentType.")
-    networkParameters: Dict[str, Union[str, int, float, bool, list, None]] = Field(description="Parameter to creat the neural Netowrk inside the agent.") # TODO: link sb3 webside with furthe information
+    baseParameters: Dict[str, Union[str, int, float, bool, None]] = Field(
+        description="Parameters every sb3 agent has in common.")
+    agentParameters: Dict[str, Union[str, int, float, bool, None]] = Field(
+        description="Special parameters specific to agentType.")
+    networkParameters: Dict[str, Union[str, int, float, bool, list, None]] = Field(
+        description="Parameter to creat the neural Netowrk inside the agent.")  # TODO: link sb3 webside with furthe information
 
-    selfPlayParameters: Optional[SelfPlayParameters] = Field("Parameters to use for self play. Only required if you want to use self play.")
+    selfPlayParameters: Optional[SelfPlayParameters] = Field(
+        "Parameters to use for self play. Only required if you want to use self play.")
     environmentParameters: EnvironmentParameters = Field("Parameters needed to creat the gym.Env")
 
 
@@ -259,14 +301,21 @@ async def train_agent_one_episode(background_tasks: BackgroundTasks):
     background_tasks.add_task()
 
 
+class PredictRequest(BaseModel):
+    observation: list[int] = Field("Observation without any preprocessing.")
+    availableActions: list[int] = Field("Available actions for Action Mask: List of actions (ints) that are available")
+
+
 @app.post("/agents/{agent_id}/predict")
-async def predict(agent_id: UUID, observation: list[int]):
+async def predict(agent_id: UUID, predictRequest: PredictRequest):
     global agents
     agent = agents[agent_id]
-    with agent.use_model_lock:
-        action = agents[agent_id].baseAlgorithm.predict(np.array(observation))[0]
-    print(f"predict {action}")
-    return Response(f"{action}", media_type="text/plain")
+
+    action_with_values = agents[agent_id].predict(np.array(predictRequest.observation), np.array(predictRequest.availableActions))
+    response = {"action": int(action_with_values[0]), #TODo: base object
+                "actionValues": action_with_values[1]}
+    print(f"predict {action_with_values}")
+    return JSONResponse(response)
 
 
 @app.post("/agents/{agent_id}/save")
@@ -286,10 +335,13 @@ async def load(agent_id: UUID, load_parameter: LoadParameter):
     print(load_parameter.agentType)
     path = os.path.dirname(os.path.abspath(__file__))
     match load_parameter.agentType:
-        case "DQN": agents[agent_id] = Agent(agent_id, DQN.load(path + f"/saved_models/{str(agent_id)}"))
-        case "PPO": agents[agent_id] = Agent(agent_id, PPO.load(path + f"/saved_models/{str(agent_id)}"))
+        case "DQN":
+            agents[agent_id] = Agent(agent_id, DQN.load(path + f"/saved_models/{str(agent_id)}"))
+        case "PPO":
+            agents[agent_id] = Agent(agent_id, PPO.load(path + f"/saved_models/{str(agent_id)}"))
     print(agent_id)
     print(agents)
+
 
 @app.post("/agents/{agent_id}/selfPlay")
 async def selfPlay(agent_id: UUID, observation: list[int]):
