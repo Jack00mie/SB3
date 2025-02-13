8dfb5180d57df92a6a7f6274e0a1aa73bff0b736
LeonP <leon.pueschel@gmx.de>
2025-02-13 13:15:25 +0100

    first part of safe best model

--- httpEnvironment.py
+++ httpEnvironment.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import signal
 import threading
@@ -13,7 +14,7 @@ import requests
 from pydantic import BaseModel, Field
 from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.base_class import BaseAlgorithm
-from stable_baselines3.common.callbacks import BaseCallback, EveryNTimesteps
+from stable_baselines3.common.callbacks import BaseCallback, EveryNTimesteps, EvalCallback
 from stable_baselines3.common.preprocessing import preprocess_obs
 import torch as th
 from fastapi import BackgroundTasks, FastAPI, Response
@@ -46,10 +47,21 @@ class SelfPlayParameters(BaseModel):
     """
     The Parameters used for self play.
     """
-    policyWindowSize: int = Field("How many old policy's/ models should be saved.", gt=0)
-    addPolicyEveryXSteps: int = Field("Add current the policy every X steps. Determines how old policy should be are.",
+    policyWindowSize: int = Field(description="How many old policy's/ models should be saved.", gt=0)
+    addPolicyEveryXSteps: int = Field(description="Add current the policy every X steps. Determines how old policy should be are.",
                                       gt=0)
-    useLatestPolicy: float = Field("How often should the latest policy be used for self play.", ge=0, le=1)
+    useLatestPolicy: float = Field(description="How often should the latest policy be used for self play.", ge=0, le=1)
+
+
+class EvaluationOptions(BaseModel):
+    """
+    The Options used for evaluation.
+    """
+    evaluateEveryEpisodes: int = Field(description="How often should be evaluated.", gt=0)
+    numberOfGames: int = Field(description="Number of games for evaluation.", gt=0)
+    opponent: str = Field(description="Name of opponent used for evaluation.")
+    safeBest: bool = Field(description="Safe Policy if better")
+
 
 
 class Agent:
@@ -58,29 +70,17 @@ class Agent:
     self_play_polices: deque[BasePolicy] = None
     latest_policy: BasePolicy = None
     use_self_play: bool = False
-    callback: BaseCallback = None
     use_latest_policy = 0.2
 
     use_model_lock = threading.Lock()
 
-    def __init__(self, agent_id: UUID, model: BaseAlgorithm, self_play_parameters: [SelfPlayParameters | None] = None):
+    def __init__(self, agent_id: UUID, model: BaseAlgorithm):
         self.agent_id = agent_id
         self.baseAlgorithm = model
 
-        if self_play_parameters is not None:
-            self.use_self_play = True
-            self.latest_policy = copy.deepcopy(self.baseAlgorithm.policy)
-            self.self_play_polices = deque(maxlen=self_play_parameters.policyWindowSize - 1)
-            for _ in range(self_play_parameters.policyWindowSize - 1):
-                self.self_play_polices.append(self.latest_policy)
-            add_to_self_play = AddToSelfPlay(self)
-            self.callback = EveryNTimesteps(n_steps=self_play_parameters.addPolicyEveryXSteps,
-                                            callback=add_to_self_play)
-            self.use_latest_policy = self_play_parameters.useLatestPolicy
-
     @classmethod
     def form_parameters(cls, agent_id, env: gym.Env, agent: str, base_parameters: dict, agent_parameters: dict,
-                        network_parameters: dict, self_play_parameters: [SelfPlayParameters | None] = None):
+                        network_parameters: dict):
 
         if "activation_fn" in network_parameters:
             activation_fn = None
@@ -111,50 +111,90 @@ class Agent:
                 model = PPO("MlpPolicy", env, policy_kwargs=network_parameters, **base_parameters, **agent_parameters)
 
         print(model.policy)
-        return cls(agent_id, model, self_play_parameters)
+        return cls(agent_id, model)
 
-    def learn(self, total_time_steps: int):
+    def learn(self, total_time_steps: int, evaluation_options: EvaluationOptions, self_play_parameters: [SelfPlayParameters | None] = None):
         print(total_time_steps)
-        self.baseAlgorithm.learn(total_time_steps, callback=self.callback)
+        # prepare callbacks
+        callbacks: list[BaseCallback]= []
+        # update self play callback
+        if self_play_parameters is not None:
+            callbacks.append(self.prepare_for_self_play(self_play_parameters))
+
+        if evaluation_options is not None:
+            callbacks.append(self.prepare_for_evalutaion(evaluation_options))
+
+        self.baseAlgorithm.learn(total_time_steps, callback=callbacks)
         requests.post(f"http://127.0.0.1:{JAVA_PORT}/trainingFinished", "Training finished")
         print("Training complete.")
 
-    def self_play(self, observation: np.ndarray):
+    def safe_model_if_better(self, win_rate: float):
+        print(win_rate)
+        # TODO
+
+    def safe_eval_results_to_tensorboard(self, win_rate: float):
+        print(win_rate)
+        # TODO
+
+    def prepare_for_self_play(self, self_play_parameters: SelfPlayParameters) -> EveryNTimesteps:
+        self.use_self_play = True
+        self.latest_policy = copy.deepcopy(self.baseAlgorithm.policy)
+        self.self_play_polices = deque(maxlen=self_play_parameters.policyWindowSize - 1)
+        for _ in range(self_play_parameters.policyWindowSize - 1):
+            self.self_play_polices.append(self.latest_policy)
+        add_to_self_play = AddToSelfPlay(self)
+        add_to_self_play_callback = EveryNTimesteps(n_steps=self_play_parameters.addPolicyEveryXSteps,
+                                                    callback=add_to_self_play)
+        self.use_latest_policy = self_play_parameters.useLatestPolicy
+        return add_to_self_play_callback
+
+    def prepare_for_evalutaion(self, evaluation_options: EvaluationOptions) -> BaseCallback:
+        callback = EveryNTimesteps(n_steps=evaluation_options.evaluateEveryEpisodes,
+                                   callback=EvaluationCallback(self, evaluation_options))
+        return callback
+
+    def get_self_play_policy(self) -> BasePolicy:
         if random.random() < self.use_latest_policy:
-            policy = self.latest_policy
+            return self.latest_policy
         else:
-            policy = random.choice(self.self_play_polices)
-        action = policy.predict(observation)
+            return random.choice(self.self_play_polices)
 
-        print(action)
-
-        return action
-
-    def predict_values(self, observation: np.ndarray) -> np.ndarray:
+    def predict_values(self, observation: np.ndarray, policy: BasePolicy) -> np.ndarray:
         if type(self.baseAlgorithm) is DQN:
             with th.no_grad():
-                tensor = self.baseAlgorithm.policy.obs_to_tensor(observation)[0]
-                values = self.baseAlgorithm.policy.q_net(tensor)
+                tensor = policy.obs_to_tensor(observation)[0]
+                values = policy.q_net(tensor)
                 values = values.detach().to('cpu').numpy()
         elif type(self.baseAlgorithm) is PPO:
             with th.no_grad():
-                tensor = self.baseAlgorithm.policy.obs_to_tensor(observation)[0]
-                distribution = self.baseAlgorithm.policy.get_distribution(tensor)
+                tensor = policy.obs_to_tensor(observation)[0]
+                distribution = policy.get_distribution(tensor)
                 probs = distribution.distribution.probs
                 values = probs.detach().to('cpu').numpy()
 
         print(f"action values: {values[0]}")
         return values[0]
 
-    def predict(self, observation: np.ndarray, available_actions: np.ndarray) -> tuple[int, list[float]]:
-        values = self.predict_values(observation)
+    def predict(self, observation: np.ndarray, available_actions: np.ndarray, deterministic: bool = True, for_self_play: bool = False) -> tuple[int, list[float]]:
+        policy = self.baseAlgorithm.policy
+        if for_self_play:
+            policy = self.get_self_play_policy()
+
+        values = self.predict_values(observation, policy)
+
         available_values: list[float] = []
-        best_action: int = available_actions[0]
+        best_action: int = int(available_actions[0])
 
         for available_action in available_actions:
             if values[available_action] > values[best_action]:
                 best_action = available_action
             available_values.append(float(values[available_action]))
+
+        if not deterministic and len(available_actions) > 1:
+            probabilities = np.array(available_values)
+            probabilities = probabilities + abs(probabilities.min())
+            probabilities = probabilities / sum(probabilities)
+            best_action = np.random.choice(available_actions, p=probabilities)
 
         print(f"availbae actions {available_actions}")
         print(values)
@@ -162,6 +202,7 @@ class Agent:
         print(best_action)
 
         return int(best_action), available_values
+
 
 
 class AddToSelfPlay(BaseCallback):
@@ -174,6 +215,25 @@ class AddToSelfPlay(BaseCallback):
     def __init__(self, agent: Agent, verbose: int = 0):
         super().__init__(verbose)
         self.agent = agent
+
+class EvaluationCallback(BaseCallback):
+    def __init__(self, agent: Agent, evaluation_options: EvaluationOptions, verbose: int = 0):
+        super().__init__(verbose)
+        self.agent = agent
+        self.evaluation_options = evaluation_options
+
+    def _on_step(self) -> bool:
+        response = requests.post(f"http://127.0.0.1:{JAVA_PORT}/eval", json={
+            "opponentName": self.evaluation_options.opponent,
+            "numberOfGames": self.evaluation_options.numberOfGames
+        })
+        print("WINRATE: ")
+        print(response.json())
+        win_rate: float = response.json()["winRate"]
+        if self.evaluation_options.safeBest:
+            self.agent.safe_model_if_better(win_rate)
+        self.agent.safe_eval_results_to_tensorboard(win_rate)
+        return True
 
 
 # TODO: WHere put env and trainer?
@@ -220,7 +280,7 @@ class HttpEnv(gym.Env):
 
         observation_vector = np.array(response_body["observationVector"])
         reward = response_body["reward"]
-        terminated = response_body["terminated"]
+        terminated = response_body["terminated"] # TODO: BaseModel
         truncated = response_body["truncated"]
         info = response_body["info"]
         # print("step: ")
@@ -246,10 +306,7 @@ class SB3Parameters(BaseModel):
         description="Special parameters specific to agentType.")
     networkParameters: Dict[str, Union[str, int, float, bool, list, None]] = Field(
         description="Parameter to creat the neural Netowrk inside the agent.")  # TODO: link sb3 webside with furthe information
-
-    selfPlayParameters: Optional[SelfPlayParameters] = Field(
-        "Parameters to use for self play. Only required if you want to use self play.")
-    environmentParameters: EnvironmentParameters = Field("Parameters needed to creat the gym.Env")
+    environmentParameters: EnvironmentParameters = Field(description="Parameters needed to creat the gym.Env")
 
 
 @app.post("/agents")
@@ -265,19 +322,19 @@ async def create_agent(sb3_parameters: SB3Parameters):
     print(sb3_parameters.baseParameters)
     print(sb3_parameters.agentParameters)
     print(sb3_parameters.networkParameters)
-    print(sb3_parameters.selfPlayParameters)
     env = HttpEnv(sb3_parameters.environmentParameters)
     agents[sb3_parameters.agent_id] = Agent.form_parameters(sb3_parameters.agent_id, env, sb3_parameters.agentType,
                                                             sb3_parameters.baseParameters,
                                                             sb3_parameters.agentParameters,
-                                                            sb3_parameters.networkParameters,
-                                                            sb3_parameters.selfPlayParameters)
+                                                            sb3_parameters.networkParameters)
     return Response("Env created", media_type="text/plain")
 
 
 class LearningParameters(BaseModel):
     totalTimeSteps: int = Field(gt=0)
-
+    evaluationOptions: EvaluationOptions
+    selfPlayParameters: Optional[SelfPlayParameters] = Field(
+        "Parameters to use for self play. Only required if you want to use self play.")
 
 # starts training Agent for total number of time steps
 @app.post("/agents/{agent_id}/learn")
@@ -290,8 +347,12 @@ async def learn(agent_id: UUID, learning_parameters: LearningParameters, backgro
     :return:
     """
     global agents
-    print(learning_parameters.totalTimeSteps)
-    background_tasks.add_task(agents[agent_id].learn, total_time_steps=learning_parameters.totalTimeSteps)
+    print("--- Learning Parameters ---")
+    print(f"total episodes: {learning_parameters.totalTimeSteps}")
+    print(learning_parameters.selfPlayParameters)
+    print(learning_parameters.evaluationOptions)
+    print("---------------------------")
+    background_tasks.add_task(agents[agent_id].learn, total_time_steps=learning_parameters.totalTimeSteps, self_play_parameters=learning_parameters.selfPlayParameters, evaluation_options=learning_parameters.evaluationOptions)
     return Response("training started", media_type="text/plain")
 
 
@@ -302,51 +363,77 @@ async def train_agent_one_episode(background_tasks: BackgroundTasks):
 
 
 class PredictRequest(BaseModel):
-    observation: list[int] = Field("Observation without any preprocessing.")
-    availableActions: list[int] = Field("Available actions for Action Mask: List of actions (ints) that are available")
+    observation: list[int] = Field(description="Observation without any preprocessing.")
+    availableActions: list[int] = Field(description="Available actions for Action Mask: List of actions (ints) that are available")
+    deterministic: bool = Field(description="If ture takes get the Action with the maximum value, if false get a action with the probepility of the value.", default=True)
 
 
 @app.post("/agents/{agent_id}/predict")
-async def predict(agent_id: UUID, predictRequest: PredictRequest):
+async def predict(agent_id: UUID, predict_request: PredictRequest):
     global agents
     agent = agents[agent_id]
 
-    action_with_values = agents[agent_id].predict(np.array(predictRequest.observation), np.array(predictRequest.availableActions))
+    action_with_values = agents[agent_id].predict(np.array(predict_request.observation), np.array(predict_request.availableActions), predict_request.deterministic)
     response = {"action": int(action_with_values[0]), #TODo: base object
                 "actionValues": action_with_values[1]}
     print(f"predict {action_with_values}")
     return JSONResponse(response)
 
 
+DATE_FORMAT: str = "%d_%m_%Y,%H_%M_%S"
+
+def get_date_and_time() -> str:
+    now = datetime.now()
+    dt_string = now.strftime(DATE_FORMAT)
+    return dt_string
+
+def get_latest_date_string(date_strings):
+    return max(date_strings, key=lambda x: datetime.strptime(x, DATE_FORMAT))
+
+
+class SafeRequest(BaseModel):
+    agentType: str = Field(description="Name of the agent. e.g: PPO, DQN.")
+    gameName: str = Field(description="Name of the game. e.g: TTT, C4.")
+
+
 @app.post("/agents/{agent_id}/save")
-async def save(agent_id: UUID):
+async def save(agent_id: UUID, save_request: SafeRequest): # TODO: put agetntype GaemName in creat
     global agents
-    path = os.path.dirname(os.path.abspath(__file__))
-    agents[agent_id].baseAlgorithm.save(path + f"/saved_models/{str(agent_id)}")
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    agents[agent_id].baseAlgorithm.save(root_dir + f"/saved_models/{save_request.gameName}/{save_request.agentType}/{str(agent_id)}/{get_date_and_time()}")
 
 
-class LoadParameter(BaseModel):
-    agentType: str
+class LoadRequest(BaseModel):
+    agentType: str = Field(description="Name of the agent. e.g: PPO, DQN.")
+    gameName: str = Field(None, description="Name of the game. e.g: TTT, C4.")
+    path: str = Field(None, description="Load a specific model. If None load latest.")
 
 
 @app.post("/agents/{agent_id}/load")
-async def load(agent_id: UUID, load_parameter: LoadParameter):
+async def load(agent_id: UUID, load_request: LoadRequest):
     global agents
-    print(load_parameter.agentType)
-    path = os.path.dirname(os.path.abspath(__file__))
-    match load_parameter.agentType:
+    print(load_request.agentType)
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    if load_request.path is None:
+        files = os.listdir()
+        latest = get_latest_date_string(files)
+        path = root_dir + f"/saved_models/{load_request.gameName}/{load_request.agentType}/{str(agent_id)}/{latest}"
+    else:
+        path = load_request.path
+
+    match load_request.agentType:
         case "DQN":
-            agents[agent_id] = Agent(agent_id, DQN.load(path + f"/saved_models/{str(agent_id)}"))
+            agents[agent_id] = Agent(agent_id, DQN.load(path))
         case "PPO":
-            agents[agent_id] = Agent(agent_id, PPO.load(path + f"/saved_models/{str(agent_id)}"))
+            agents[agent_id] = Agent(agent_id, PPO.load(path))
     print(agent_id)
     print(agents)
 
 
 @app.post("/agents/{agent_id}/selfPlay")
-async def selfPlay(agent_id: UUID, observation: list[int]):
+async def selfPlay(agent_id: UUID, predict_request: PredictRequest):
     global agents
-    action = agents[agent_id].self_play(np.array(observation))[0]
+    action = agents[agent_id].predict(np.array(predict_request.observation), np.array(predict_request.availableActions), predict_request.deterministic, True)[0]
     print(f"self play: {action}")
     return Response(f"{action}", media_type="text/plain")
 
